313c567f76338591e0971add06d3057c4fd54bc4
Daniel Santana <Santana.danny4g@gmail.com>
2021-04-18 22:15:39 -0700

    Login functionality complete, working on Register

--- app/src/main/java/com/codepath/appointsy/LoginActivity.java
+++ app/src/main/java/com/codepath/appointsy/LoginActivity.java
@@ -5,45 +5,66 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.codepath.appointsy.databinding.ActivityLoginBinding;
-import com.codepath.appointsy.fragments.BusinessFragment;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private final String TAG = "LoginActivity";
     private ActivityLoginBinding binding;
     private MaterialButton btnLogin;
     private MaterialButton btnRegister;
+    private RelativeLayout rlLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
         // ref. views
         btnLogin = binding.btnLogin;
         btnRegister = binding.btnRegister;
+        rlLogin = binding.rlLogin;
 
         // user selects to login, check pass & email
         btnLogin.setOnClickListener((e) -> {
-            //TODO Parse login
-            // handle error with SnackBar rather than a Toast
-
-            // if login successful route to Business
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            // remove stack call backs to prevent user from going back to register/login
-            finishAffinity();
+            // Perform API call
+            // TODO check if inputs are not null
+            String username = binding.tiUsername.getEditText().getText().toString();
+            String pass = binding.tiPassword.getEditText().getText().toString();
+            Toast.makeText(this, username + " " + pass, Toast.LENGTH_SHORT).show();
+            loginUser(username, pass);
         });
+
         // User decides to register
         btnRegister.setOnClickListener((e) -> {
             Intent intent = new Intent(this, RegisterActivity.class);
             startActivity(intent);
         });
+    }
 
-
-
+    // handle user login
+    private void loginUser(String username, String pass) {
+        ParseUser.logInInBackground(username, pass, (user, err) -> {
+            // login successful, user returned
+            if(user != null){
+                // login successful route to Business
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                // remove stack call backs to prevent user from going back to register/login
+                finishAffinity();
+            } else {
+                // login failed
+                Snackbar.make(rlLogin, "Failed to login. Try again.", Snackbar.LENGTH_SHORT).show();
+                Log.e(TAG, "Error logging", err);
+            }
+        });
     }
 }
--- app/src/main/java/com/codepath/appointsy/RegisterActivity.java
+++ app/src/main/java/com/codepath/appointsy/RegisterActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.codepath.appointsy.databinding.ActivityRegisterBinding;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.android.material.snackbar.Snackbar;
+import com.parse.ParseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,6 +18,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private MaterialCheckBox cbBusiness;
     private MaterialButton btnConfirm;
+    private LinearLayout llRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +27,37 @@ public class RegisterActivity extends AppCompatActivity {
 
         cbBusiness = binding.cbBusiness;
         btnConfirm = binding.btnConfirm;
+        llRegister = binding.llRegister;
 
         btnConfirm.setOnClickListener((e) -> {
-
-            /* can convert to ternary operator instead */
-
-            // TODO navigate to respective setup profiles, pass the current Inputs
-            // for auto fill
-
-            // if cb is not selected
-            if(!cbBusiness.isChecked()){
-                // navigate to client profile
-            }
-            else {
-                // cb is checked, navigate to business profile
-            }
+            ParseUser user = new ParseUser();
+            // TODO check if inputs are not null
+            user.setUsername(binding.tiUsername.getEditText().toString());
+            user.setEmail(binding.tiEmail.getEditText().toString());
+            user.setPassword(binding.tiPassword.getEditText().toString());
+            user.put("fullName", binding.tiName.getEditText().toString());
+            user.put("phoneNumber", binding.tiPassword.getEditText().getText());
+            signUpFunc(user);
         });
 
+    }
+
+    private void signUpFunc(ParseUser user) {
+        user.signUpInBackground((e) -> {
+            // check if sign up has errors
+            if(e != null){
+                Snackbar.make(llRegister, "Register failed.", Snackbar.LENGTH_SHORT).show();
+            } else {
+                // register successful
+                // if cb is not selected
+                if(!cbBusiness.isChecked()){
+                    // navigate to client profile
+                    // TODO navigate to respective setup profiles, pass the current Inputs through Intents
+                }
+                else {
+                    // cb is checked, navigate to business profile
+                }
+            }
+        });
     }
 }
