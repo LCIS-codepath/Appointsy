f7dd102266147bff2e8b158cce81955f526d3153
BigDanVader <dluoma1980@gmail.com>
2021-04-22 19:50:15 -0700

    Started ProfileFragment and BusinessProfileFragment implementation

--- app/src/main/java/com/codepath/appointsy/fragments/BusinessProfileFragment.java
+++ app/src/main/java/com/codepath/appointsy/fragments/BusinessProfileFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -15,40 +13,44 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.codepath.appointsy.R;
-import com.codepath.appointsy.databinding.ActivityRegisterBinding;
+import com.codepath.appointsy.databinding.FragmentBusinessBinding;
+import com.codepath.appointsy.databinding.FragmentBusinessProfileBinding;
 import com.codepath.appointsy.databinding.FragmentProfileBinding;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.android.material.textview.MaterialTextView;
 import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
+ * Use the {@link BusinessProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class BusinessProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private FragmentProfileBinding binding;
+
+    private FragmentBusinessProfileBinding binding;
     private ImageView ivProfile;
     private TextInputLayout tvName;
     private EditText etName;
-    private TextInputLayout tvUsername;
+    private TextInputLayout tvOwner;
+    private EditText etOwner;
     private TextInputLayout tvEmail;
+    private EditText etEmail;
     private TextInputLayout tvPassword;
+    private EditText etPassword;
     private TextInputLayout tvBio;
+    private EditText etBio;
     private MaterialButton btUpdate;
-
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public ProfileFragment() {
+    public BusinessProfileFragment() {
         // Required empty public constructor
     }
 
@@ -58,11 +60,11 @@ public class ProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
+     * @return A new instance of fragment BusinessProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
+    public static BusinessProfileFragment newInstance(String param1, String param2) {
+        BusinessProfileFragment fragment = new BusinessProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -83,7 +85,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        binding = FragmentBusinessProfileBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
@@ -94,12 +96,19 @@ public class ProfileFragment extends Fragment {
         ParseUser user = ParseUser.getCurrentUser();
         ivProfile = binding.ivProfile;
         tvName = binding.tvName;
-        tvUsername = binding.tvUsername;
+        tvOwner = binding.tvOwner;
         tvEmail = binding.tvEmail;
         tvPassword = binding.tvPassword;
         tvBio = binding.tvBio;
         btUpdate = binding.btUpdate;
         etName = binding.etName;
+        etOwner = binding.etOwner;
+        etEmail = binding.etEmail;
+        etPassword = binding.etPassword;
+        etBio = binding.etBio;
+
+        //Set text for each field
+        //Ex: etName.setText(user.getBusinessName());
 
         btUpdate.setOnClickListener((e ->{
 
