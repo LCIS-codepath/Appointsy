d626112589ad6a31386842005ffbecdf847ff9ba
Edward <eisajanyan@tuta.io>
2021-05-14 13:52:30 -0700

    renamed file to a more descriptive name + added implementation file

---
+++ app/src/main/java/com/codepath/appointsy/BusinessDetails.java
@@ -0,0 +1,15 @@
+package com.codepath.appointsy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusinessDetails extends AppCompatActivity {
+    private
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_details);
+    }
+}
