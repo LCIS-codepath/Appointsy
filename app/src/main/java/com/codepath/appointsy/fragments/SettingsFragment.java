f997ca1a1ee947f024b323e1d33519026e49687c
Daniel Santana <Santana.danny4g@gmail.com>
2021-05-28 22:30:47 -0700

    Updated settings UI, added darkmode, and cleaned files

--- app/src/main/java/com/codepath/appointsy/fragments/SettingsFragment.java
+++ app/src/main/java/com/codepath/appointsy/fragments/SettingsFragment.java
@@ -5,43 +5,31 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
+
+import android.widget.TextView;
 
 import com.codepath.appointsy.LoginActivity;
-import com.codepath.appointsy.R;
-import com.codepath.appointsy.databinding.FragmentBusinessProfileBinding;
 import com.codepath.appointsy.databinding.FragmentSettingsBinding;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.switchmaterial.SwitchMaterial;
-import com.google.android.material.textfield.TextInputLayout;
 import com.parse.ParseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.jetbrains.annotations.NotNull;
+
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private FragmentSettingsBinding binding;
-    private TextInputLayout tvDarkMode;
-    private TextInputLayout tvNotifications;
-    private TextInputLayout tvCredits;
-    private TextInputLayout tvVersion;
-    private TextInputLayout tvCopyright;
+    private TextView tvDarkMode;
+    private TextView tvNotifications;
+    private TextView tvCredits;
+    private TextView tvVersion;
+    private TextView tvCopyright;
     private SwitchMaterial swDarkMode;
     private SwitchMaterial swNotifications;
     private MaterialButton btnRelease;
@@ -53,20 +41,9 @@ public class SettingsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
+    public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -74,14 +51,10 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
@@ -112,60 +85,46 @@ public class SettingsFragment extends Fragment {
 
         //TODO Determine where Credits and Version come from
 
-        swDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    //Set DarkMode variable to True
-                } else {
-                    // The toggle is disabled
-                    // Set DarkMode variable to False
-                }
+        swDarkMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                // The toggle is enabled
+                //Set DarkMode variable to True
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                // The toggle is disabled
+                // Set DarkMode variable to False
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
         });
 
-        swNotifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    //Set Notifications variable to True
-                } else {
-                    // The toggle is disabled
-                    // Set Notifications variable to False
-                }
+        swNotifications.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                // The toggle is enabled
+                //Set Notifications variable to True
+            } else {
+                // The toggle is disabled
+                // Set Notifications variable to False
             }
         });
 
-        btnRelease.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO Determine what Release actually does
-            }
+        btnRelease.setOnClickListener(v -> {
+            //TODO Determine what Release actually does
         });
 
-        btnAbout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO Determine what About actually does
-            }
+        btnAbout.setOnClickListener(v -> {
+            //TODO Determine what About actually does
         });
 
-        btnContact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO Determine what Contact actually does
-            }
+        btnContact.setOnClickListener(v -> {
+            //TODO Determine what Contact actually does
         });
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParseUser.logOut();
-                Intent i = new Intent(getContext(), LoginActivity.class)
-                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(i);
-            }
+        btnLogout.setOnClickListener(v -> {
+            ParseUser.logOut();
+            Intent i = new Intent(getContext(), LoginActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
         });
     }
 }
